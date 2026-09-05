d85852f2662e2164e6b3aeac8971e47ecdda8342
Ankit Tadiyal <tadiyalankit2005@gmail.com>
2026-09-05 21:01:44 +0530

    Time: 0 ms (100%), Space: 12.5 MB (51.47%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
-        int i = m - 1;      // Pointer for the last valid element in nums1
-        int j = n - 1;      // Pointer for the last element in nums2
-        int k = m + n - 1;  // Pointer for the last available slot in nums1
-        
-        // Merge in reverse order
+        int i = m - 1;      // pointer for the last valid element in nums1
+        int j = n - 1;      // pointer for the last element in nums2
+        int k = m + n - 1;  // pointer for the last available slot in nums1
+
+        // merge in reverse order
         while (j >= 0) {
             if (i >= 0 && nums1[i] > nums2[j]) {
                 nums1[k--] = nums1[i--];
