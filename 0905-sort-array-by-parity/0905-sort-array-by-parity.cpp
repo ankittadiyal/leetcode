196b7e7207681aafd595902c5e4659a6dc674e09
Ankit Tadiyal <230864539+ankittadiyal@users.noreply.github.com>
2026-09-11 23:38:09 +0530

    Time: 0 ms (100%), Space: 20.5 MB (79.64%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -5,12 +5,10 @@ public:
         int right = nums.size() - 1;
 
         while (left < right) {
-            // Agar left odd hai aur right even hai, to swap karein
             if (nums[left] % 2 > nums[right] % 2) {
                 swap(nums[left], nums[right]);
             }
 
-            // Pointers ko aage badhayein
             if (nums[left] % 2 == 0) {
                 left++;
             }
