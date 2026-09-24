31ae41319d2eb4d71497ad5694f00939282321ee
Ankit Tadiyal <230864539+ankittadiyal@users.noreply.github.com>
2026-09-24 23:40:10 +0530

    Time: 0 ms (100%), Space: 9.5 MB (76.35%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.cpp
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -4,12 +4,9 @@ public:
         int n = s.length();
         
         for (int i = 0; i < n; i += 2 * k) {
-            // Pointers setup karein
             int left = i;
-            // Agar bache hue characters k se kam hain, toh n - 1 tak hi reverse hoga
             int right = (i + k - 1 < n - 1) ? i + k - 1 : n - 1;
             
-            // Khud se characters ko swap karein (bina kisi library function ke)
             while (left < right) {
                 char temp = s[left];
                 s[left] = s[right];
