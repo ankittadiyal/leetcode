e816ce5e4903b7eb29af08f6825b25a2eb515b76
Ankit Tadiyal <230864539+ankittadiyal@users.noreply.github.com>
2026-09-25 20:10:44 +0530

    Time: 0 ms (100%), Space: 7.7 MB (91.41%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,7 +1,6 @@
 class Solution {
  public:
   string convertToTitle(int n) {
-    return n == 0 ? ""
-                  : convertToTitle((n - 1) / 26) + (char)('A' + ((n - 1) % 26));
+    return n == 0 ? "": convertToTitle((n - 1) / 26) + (char)('A' + ((n - 1) % 26));
   }
 };
